04259b7cc94256788920cc8d2ba098bd793b2c04
cc-chen-tech <maxminicherry@gmail.com>
2026-07-29 06:00:02 +0800

    feat: transfer actual zero package energy to the full PNT error

---
+++ Test/ZeroDensityLayerBudgetActualZeroPackageNaturalPNTLowerTransferAxiomAudit.lean
@@ -0,0 +1,5 @@
+import PrimeNumberTheorem.ZeroDensityLayerBudgetActualZeroPackageNaturalPNTLowerTransfer
+
+#print axioms PrimeNumberTheorem.NaturalPointTargetAmplitudeNegligible.const_mul_amplitude
+#print axioms PrimeNumberTheorem.actualZeroPackage_naturalPointRemainder_lowerTransfer
+
