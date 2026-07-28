63c1949e938073faa12d35bbcd4f3f1f66d9a07a
cc-chen-tech <maxminicherry@gmail.com>
2026-07-29 03:44:56 +0800

    feat: connect window energies to actual PNT transfer

---
+++ Test/ZeroDensityLayerBudgetActualCarlsonWindowEnergyBidirectionalPNTTransferAxiomAudit.lean
@@ -0,0 +1,5 @@
+import PrimeNumberTheorem.ZeroDensityLayerBudgetActualCarlsonWindowEnergyBidirectionalPNTTransfer
+
+open PrimeNumberTheorem
+
+#print axioms exists_zeroSupportedActualCarlsonFiniteSeedWindowEnergyBidirectionalPNTCanonicalSharpRealTransfer
