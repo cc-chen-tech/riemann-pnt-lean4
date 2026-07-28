fc5f0edc90ab921f73f6c90cc29aaf0aebb04873
cc-chen-tech <maxminicherry@gmail.com>
2026-07-28 18:50:49 +0800

    feat: transfer balanced Carlson cluster witnesses to PNT
    (cherry picked from commit 1fd3f9c683401f1c16cde9bcee3643fc77a56d94)

---
+++ Test/ZeroDensityLayerBudgetActualCarlsonBalancedPNTLowerTransferAxiomAudit.lean
@@ -0,0 +1,5 @@
+import PrimeNumberTheorem.ZeroDensityLayerBudgetActualCarlsonBalancedPNTLowerTransfer
+
+open PrimeNumberTheorem
+
+#print axioms selectedUniformGoodHeightActualCarlsonBalancedPNTLowerTransfer_automatic
