4da7de68ad29c15f0d91e9e341ea76eaf267c0b5
cc-chen-tech <maxminicherry@gmail.com>
2026-08-01 06:43:41 +0800

    feat: automate canonical polynomial upper and signed transfer

---
+++ PrimeNumberTheorem/ZeroDensityLayerBudgetCanonicalPolynomialUnifiedUpperSignedOmegaContract.lean
@@ -0,0 +1,3 @@
+import PrimeNumberTheorem.ZeroDensityLayerBudgetCanonicalPolynomialUnifiedUpperSignedOmega
+
+#check PrimeNumberTheorem.actualCanonicalPolynomialVariableBoundaryUnifiedUpperSignedOmega
