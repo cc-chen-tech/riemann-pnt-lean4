ef6bc030ccd485b3016cbee737363c2194a74cd2
cc-chen-tech <maxminicherry@gmail.com>
2026-08-01 16:15:33 +0800

    Audit square capacity deletion bound
    (cherry picked from commit 723224e066ac1cef3cda1ea433433e6c286487f2)

--- Test/ExceptionalZeroDyadicSquareMultiplicityContract.lean
+++ Test/ExceptionalZeroDyadicSquareMultiplicityContract.lean
@@ -6,6 +6,11 @@ open scoped BigOperators
 namespace PrimeNumberTheorem
 
 #check squareMultiplicityCapacity_le_max_mul_linearMultiplicityCapacity
+#check
+  (squareMultiplicityCapacity_sdiff_le :
+    ∀ {α : Type*} [DecidableEq α] (R S : Finset α) (m w : α → ℝ),
+      (∀ a ∈ R, 0 ≤ w a) →
+      (∑ a ∈ R \ S, m a ^ 2 * w a) ≤ ∑ a ∈ R, m a ^ 2 * w a)
 #check squareMultiplicityCapacity_sdiff_le
 #check ExplicitFormulaAux.exists_analyticOrderNatAt_riemannZeta_le_log_im_of_nontrivialZero
 #check actualZetaDyadicZeroBlock
