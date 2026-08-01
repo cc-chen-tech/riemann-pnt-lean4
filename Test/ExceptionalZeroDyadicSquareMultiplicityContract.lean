60a8c549bdbfc66983bdd9ad8b4fcdbcdc55eae5
cc-chen-tech <maxminicherry@gmail.com>
2026-08-01 16:41:11 +0800

    Bridge dyadic zeros to unit buckets
    (cherry picked from commit 3aaf0d80b7d776eaa608e057759c280cca7691ae)

--- Test/ExceptionalZeroDyadicSquareMultiplicityContract.lean
+++ Test/ExceptionalZeroDyadicSquareMultiplicityContract.lean
@@ -5,6 +5,8 @@ open scoped BigOperators
 
 namespace PrimeNumberTheorem
 
+open VKEdgePiOverTwo
+
 #check squareMultiplicityCapacity_le_max_mul_linearMultiplicityCapacity
 #check
   (squareMultiplicityCapacity_sdiff_le :
@@ -14,6 +16,18 @@ namespace PrimeNumberTheorem
 #check squareMultiplicityCapacity_sdiff_le
 #check ExplicitFormulaAux.exists_analyticOrderNatAt_riemannZeta_le_log_im_of_nontrivialZero
 #check actualZetaDyadicZeroBlock
+#check
+  (mem_actualZetaDyadicZeroBlock_iff_exists_mem_zeroOrdinateUnitBucket :
+    ∀ {k : ℕ} {rho : ℂ},
+      rho ∈ actualZetaDyadicZeroBlock k ↔
+        ∃ n ∈ Finset.Icc (2 ^ k) (2 ^ (k + 1) - 1),
+          rho ∈ zeroOrdinateUnitBucket n)
+#check
+  (actualZetaDyadicZeroBlock_eq_biUnion_zeroOrdinateUnitBucket :
+    ∀ k : ℕ,
+      actualZetaDyadicZeroBlock k =
+        (Finset.Icc (2 ^ k) (2 ^ (k + 1) - 1)).biUnion
+          zeroOrdinateUnitBucket)
 #check actualZetaDyadicSquareReciprocalCapacityExcluding
 #check actualZetaDyadicSquareReciprocalCapacityExcluding_le_linear
 #check exists_actualZetaDyadicSquareReciprocalCapacityExcluding_le_log_linear
